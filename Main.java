e9ed1f3579cb35d72c578552dc7a7d0258864d5e
Katya_Mez <katya201206@gmail.com>
2026-03-04 15:40:58 +0000

    Created user message in Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package Guessing_Game;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        //user message
+        System.out.println("----------Welcome to the guessing game!----------");
+        System.out.println("There are 3 difficulty levels:\n");
+        System.out.println("Easy: \t1-50 numbers range, \t10 attempts");
+        System.out.println("Medium: 1-100 numbers range, \t7 attempts");
+        System.out.println("Hard: \t1-200 numbers range, \t5 attempts");
+
+    }
+}
